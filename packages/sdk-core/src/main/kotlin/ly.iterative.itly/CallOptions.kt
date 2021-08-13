2c521b9b5b1a124242077438dbb6c76afbef50d9
Dante Tam <dante.tam1@gmail.com>
2021-08-13 06:13:43 -0700

    Use composition framework for CallOptions/HashMap

--- packages/sdk-core/src/main/kotlin/ly.iterative.itly/CallOptions.kt
+++ packages/sdk-core/src/main/kotlin/ly.iterative.itly/CallOptions.kt
@@ -2,6 +2,13 @@ package ly.iterative.itly
 
 import java.util.HashMap
 
-open class CallOptions : HashMap<String, PluginCallOptions>() {
+open class CallOptions constructor(map: HashMap<String, PluginCallOptions>? = HashMap()) {
+    val map: HashMap<String, PluginCallOptions>? = map
 
+    fun get(pluginName: String): PluginCallOptions? {
+        return map?.get(pluginName)
+    }
+    fun set(pluginName: String, options: PluginCallOptions) {
+        map?.put(pluginName, options)
+    }
 }
